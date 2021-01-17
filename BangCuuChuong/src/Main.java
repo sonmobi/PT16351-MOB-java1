dcd997e3c07d9f513783b2a3a78f22c2b139ef6a
sondt32 <sondt32@fpt.edu.vn>
2021-01-17 07:52:23 +0700

    VD Mang

--- BangCuuChuong/src/Main.java
+++ BangCuuChuong/src/Main.java
@@ -3,35 +3,48 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
 
-        System.out.println("1 - Bang cuu chuong");
-        System.out.println("2 - Trung binh cong");
-        System.out.print("Chon chuc nang: ");
-        int chon;
-        int so =0;
-        Scanner sc = new Scanner(System.in);
-        chon = sc.nextInt();
-        switch (chon){
-            case 1:
-                // công việc 1
-                do{
-                    System.out.print("Nhap 1 so: ");
-                    so = sc.nextInt();
-                }while (so <2 || so >10);
-                // gọi hàm tính toán
-                TinhToan tinhToan = new TinhToan();
-                tinhToan.BangCuuChuong(so);
+//        System.out.println("1 - Bang cuu chuong");
+//        System.out.println("2 - Trung binh cong");
+//        System.out.print("Chon chuc nang: ");
+//        int chon;
+//        int so =0;
+//        Scanner sc = new Scanner(System.in);
+//        chon = sc.nextInt();
+//        switch (chon){
+//            case 1:
+//                // công việc 1
+//                do{
+//                    System.out.print("Nhap 1 so: ");
+//                    so = sc.nextInt();
+//                }while (so <2 || so >10);
+//                // gọi hàm tính toán
+//                TinhToan tinhToan = new TinhToan();
+//                tinhToan.BangCuuChuong(so);
+//
+//
+//                break;
+//
+//            case 2:
+//                // công việc 2
+//                break;
+//
+//
+//        }
 
+        //==================================
+//        // Viết chương trình in ra các số từ 1 -10 bỏ qua số chia hết cho 3
+//        for(int i = 1; i<= 10;i++){
+//
+//            if(i % 3 == 0)
+//                continue;
+//            System.out.println("So thu " + i);
+//
+//            if(i >5 )
+//                break;
+//        }
 
-                break;
-
-            case 2:
-                // công việc 2
-                break;
-
-
-        }
-
-
+        Mang mang = new Mang();
+            mang.vidu();
     }
 
 }
---
+++ BangCuuChuong/src/Mang.java
@@ -0,0 +1,28 @@
+public class Mang {
+    public void  vidu(){
+        // Khai bao mang
+        String [] dsHoTen = new String[3];
+        dsHoTen[0] ="Nguyen Van A";
+        dsHoTen[1] = "Nguyen Thi B";
+        dsHoTen[2] = "Le Van C";
+
+        for(int i = 0; i<dsHoTen.length; i++){
+            System.out.println("Phan tu thu " + i +": " + dsHoTen[i]);
+        }
+
+        // cach 2
+        for(String hoTen : dsHoTen){
+            System.out.println(hoTen);
+        }
+
+        //----- mang so nguyen
+        int[] m ={4,6,8,2};
+        String[] gioTinh = new String[3];
+
+
+
+
+
+    }
+
+}
